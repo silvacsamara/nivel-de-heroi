f06bce4170b9772a7049fa22a0077fe29934f959
silvacsamara <silvacsamara.dev@gmail.com>
2025-06-19 22:59:01 -0300

    Inclusão de nova frase ao final da execução

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// Desafio Classificador de nível de Herói
-
-// **O Que deve ser utilizado**
-// - Variáveis
-// - Operadores
-// - Laços de repetição
-// - Estruturas de decisões
-
-// Objetivo
-// Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói,
-// depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-
-// Se XP for menor do que 1.000 = Ferro
-// Se XP for entre 1.001 e 2.000 = Bronze
-// Se XP for entre 2.001 e 5.000 = Prata
-// Se XP for entre 5.001 e 7.000 = Ouro
-// Se XP for entre 7.001 e 8.000 = Platina
-// Se XP for entre 8.001 e 9.000 = Ascendente
-// Se XP for entre 9.001 e 10.000 = Imortal
-// Se XP for maior ou igual a 10.001 = Radiante
-
-// Saída
-// Ao final deve se exibir uma mensagem:
-// "O Herói de nome {nome} está no nível de {nivel}"
-
 let nomeHeroi = "Shuri";
 let quantidadeXp = 10000;
 let nivel = "";
@@ -46,3 +21,4 @@ if (quantidadeXp < 1000) {
 }
 
 console.log(`O herói de nome ${nomeHeroi} está no nível de ${nivel}!`)
+console.log("Só porque algo funciona, não significa que não possa ser melhorado!")
